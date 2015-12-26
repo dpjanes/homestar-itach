50f21609cd2c3b8e81b025af13115003c55b1ae9
David Janes <davidjanes@davidjanes.com>
2015-12-26 15:51:01 -0330

    remove old model

--- models/ITachIR.js
+++ models/ITachIR.js
@@ -8,13 +8,6 @@
 
 var iotdb = require("iotdb");
 
-exports.Model = iotdb.make_model('ITachIR')
-    .facet(":bridge.ir")
-    .name("ITachIR")
-    .description("iTach IR Controller")
-    .o("command", iotdb.string.purpose(":command"))
-    .make();
-
 exports.binding = {
     bridge: require('../ITachIRBridge').Bridge,
     model: require('./ITachIr.json'),
