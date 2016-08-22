f1f6572dda7aa9c10868fc35938f5e84d9a70381
David Janes <davidjanes@davidjanes.com>
2016-08-22 15:23:34 -0400

    remove all the 'wrap' exports that were never used

--- index.js
+++ index.js
@@ -29,8 +29,3 @@ exports.bindings = [
     require('./models/IRLGTV').binding,
     require('./models/IRSonyBDP').binding,
 ];
-
-exports.iotdb = require("iotdb");
-exports.wrap = function (name, initd) {
-    return exports.iotdb.make_wrap(name, exports.bindings, initd);
-};
