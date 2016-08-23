628096040188df8cc6e5f42654702af333970f78
David Janes <davidjanes@davidjanes.com>
2016-08-23 12:43:58 -0400

    turn on LG TV + media types

---
+++ models/ir-lg-tv-on.js
@@ -0,0 +1,24 @@
+var iotdb = require("iotdb");
+var _ = iotdb._;
+
+var cmdd = require("./data/ir-lg-tv.json");
+
+exports.binding = {
+    bridge: require('../ITachIRBridge').Bridge,
+    discover: false,
+    model: require('./ir-lg-tv-on.json'),
+    connectd: {
+        data_out: function (paramd) {
+            var ir;
+
+            paramd.rawd.irs = [];
+
+            for (var key in paramd.cookd) {
+                ir = cmdd[key];
+                if (ir) {
+                    paramd.rawd.irs.push(ir);
+                }
+            }
+        }
+    },
+};
